5e916c078d186b09c2ca0848792f203ad99258ad
klam2k20 <klam2k17@gmail.com>
2023-01-18 13:56:04 -0500

    Update DailyContentLabel content size

--- src/components/DailyContentLabel.jsx
+++ src/components/DailyContentLabel.jsx
@@ -3,9 +3,9 @@ import PropTypes from 'prop-types';
 
 function DailyContentLabel({ icon, label, content }) {
   return (
-    <span className="flex justify-center items-center gap-x-1 text-white">
+    <span className="flex justify-center items-center gap-x-1 text-white text-sm">
       {icon}
-      <span className="font-light text-sm capitalize">{label}</span>
+      <span className="font-light capitalize">{label}</span>
       <span className="font-medium">{content}</span>
     </span>
   );
--- src/components/Location.jsx
+++ src/components/Location.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function Location() {
   return (
     <div className="text-white text-3xl font-medium my-3">
-      London, GB
+      New York, NYC
     </div>
   );
 }
